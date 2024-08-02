d561878fe0b7d3948399564d5ed4902bd384622e
MayankJindal113 <mjindal1_be21@thapar.edu>
2024-08-02 22:20:55 +0530

    Updated the image

--- app/routes.py
+++ app/routes.py
@@ -42,97 +42,17 @@ def process_input():
             audio_path = os.path.join('temp', secure_filename(audio_file.filename))
             audio_file.save(audio_path)
             translated_text = translate_to_eng(audio_path)
-            response = process_text(translated_text)
+            response = model_results(translated_text)
             native_response = translate_to_native(response)
             return jsonify({
                 "response": response,
                 "native_response": native_response
             })
+    
     except Exception as e:
         current_app.logger.error(f"Error processing input: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
 
-
-# def model_results(x):
-#     # Load the fine-tuned model and tokenizer
-#     model = TFBartForConditionalGeneration.from_pretrained('app/model/kid-bart')
-#     tokenizer = BartTokenizer.from_pretrained('app/model/kid-bart')
-
-#     def preprocess_question(question, tokenizer, max_length=512):
-#         inputs = tokenizer(
-#             question,
-#             max_length=max_length,
-#             truncation=True,
-#             padding='max_length',
-#             return_tensors='tf'  # Use 'tf' for TensorFlow
-#         )
-#         return inputs
-
-#     def generate_answer(inputs, model):
-#         outputs = model.generate(
-#             input_ids=inputs['input_ids'],
-#             attention_mask=inputs['attention_mask'],
-#             max_length=150,
-#             num_beams=5,
-#             early_stopping=True,
-#             no_repeat_ngram_size=2,
-#             temperature=1.0,
-#             top_k=50,
-#             top_p=0.95
-#         )
-#         return outputs
-
-#     def decode_answer(outputs, tokenizer):
-#         answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#         return answer
-
-#     inputs = preprocess_question(x, tokenizer)
-#     outputs = generate_answer(inputs, model)
-#     answer = decode_answer(outputs, tokenizer)
-#     return answer
-
-
-# def model_results(x):
-#     # Load the fine-tuned model and tokenizer
-#     model = TFBartForConditionalGeneration.from_pretrained('app/model/kid-bart')
-#     tokenizer = BartTokenizer.from_pretrained('app/model/kid-bart')
-
-#     def preprocess_question(question, tokenizer, max_length=128):
-#         inputs = tokenizer(
-#             question,
-#             max_length=max_length,
-#             truncation=True,
-#             padding='max_length',
-#             return_tensors='tf'  # Use 'tf' for TensorFlow
-#         )
-#         return inputs
-
-#     def generate_answer(inputs, model):
-#         outputs = model.generate(
-#             input_ids=inputs['input_ids'],
-#             attention_mask=inputs['attention_mask'],
-#             max_length=150,
-#             num_beams=5,
-#             early_stopping=True,
-#             no_repeat_ngram_size=2,
-#             temperature=1.0,
-#             top_k=50,
-#             top_p=0.95
-#         )
-#         return outputs
-
-#     def decode_answer(outputs, tokenizer):
-#         answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#         return answer
-
-#     inputs = preprocess_question(x, tokenizer)
-#     outputs = generate_answer(inputs, model)
-#     answer = decode_answer(outputs, tokenizer)
-    
-#     # Clear TensorFlow session to free up memory
-#     tf.keras.backend.clear_session()
-    
-#     return answer
 
 
 def model_results(x):
@@ -173,42 +93,3 @@ def model_results(x):
     # tf.keras.backend.clear_session()
 
     return answer
-
-
-# def model_results(x):
-#     # Load the fine-tuned model and tokenizer
-#     model = BartForConditionalGeneration.from_pretrained('app/model/kid-bart')
-#     tokenizer = BartTokenizer.from_pretrained('app/model/kid-bart')
-
-#     def preprocess_question(question, tokenizer, max_length=512):
-#         inputs = tokenizer(
-#             question,
-#             max_length=max_length,
-#             truncation=True,
-#             padding='max_length',
-#             return_tensors='pt'
-#         )
-#         return inputs
-
-#     def generate_answer(inputs, model):
-#         outputs = model.generate(
-#             input_ids=inputs['input_ids'],
-#             attention_mask=inputs['attention_mask'],
-#             max_length=150,
-#             num_beams=5,
-#             early_stopping=True,
-#             no_repeat_ngram_size=2,
-#             temperature=1.0,
-#             top_k=50,
-#             top_p=0.95
-#         )
-#         return outputs
-
-#     def decode_answer(outputs, tokenizer):
-#         answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#         return answer
-
-#     inputs = preprocess_question(x, tokenizer)
-#     outputs = generate_answer(inputs, model)
-#     answer = decode_answer(outputs, tokenizer)
-#     return answer
